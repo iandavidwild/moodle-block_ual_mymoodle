ba86d8c1ce88e14c9b5c28e8cc7742ba63729e45
Ian Wild <i.wild@ulcc.ac.uk>
2012-12-17 18:05:45 +0000

    Add a new capability. Can the current user access the Admin DB tool?

---
+++ db/access.php
@@ -0,0 +1,38 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+
+/**
+ * Capabilities file
+ *
+ * @package    block
+ * @subpackage ual_mymoodle
+ * @copyright  2012 University of London Computer Centre
+ * @author     Ian Wild {@link http://moodle.org/user/view.php?id=81450}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$capabilities = array(
+
+    'block/ual_mymoodle:admin_db_link' => array(
+        'riskbitmask'  => RISK_CONFIG,
+        'captype'      => 'write',
+        'contextlevel' => CONTEXT_SYSTEM,
+        'archetypes'   => array(
+            'manager'          => CAP_ALLOW
+        )
+    )
+);
