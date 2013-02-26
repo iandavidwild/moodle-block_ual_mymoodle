3a5797fba03478aa762d604dc4f3ad5d08e44f71
ian.wild <ian.wild@heavy-horse.co.uk>
2013-02-26 15:43:07 +0000

    Option to show hidden courses is now a capability.

--- edit_form.php
+++ edit_form.php
@@ -47,9 +47,6 @@ class block_ual_mymoodle_edit_form extends block_edit_form {
         $mform->addElement('advcheckbox', 'config_showmoodlecourses', get_string('showmoodlecourses', 'block_ual_mymoodle'));
         $mform->setDefault('config_showmoodlecourses', 0);
         
-        $mform->addElement('advcheckbox', 'config_showhiddencourses', get_string('showhiddencourses', 'block_ual_mymoodle'));
-        $mform->setDefault('config_showhiddencourses', 1);
-
         $options = array(
             block_ual_mymoodle::TRIM_RIGHT => get_string('trimmoderight', 'block_ual_mymoodle'),
             block_ual_mymoodle::TRIM_LEFT => get_string('trimmodeleft', 'block_ual_mymoodle'),
