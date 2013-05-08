338fe81c02e4db039690786ded3cc28525d8ab30
Ian Wild <i.wild@ulcc.ac.uk>
2013-05-08 16:25:15 +0100

    Update version number.

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$plugin->version = 2013050801;
+$plugin->version = 2013050802;
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->requires = 2011070100; // 2.1.
 $plugin->component = 'block_ual_mymoodle';
